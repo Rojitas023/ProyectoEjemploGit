ba2b690b6bef92edabede01323fd01f5d5cbcfb5
Rojitas023 <102319361+Rojitas023@users.noreply.github.com>
2022-09-03 20:17:18 -0400

    Update nav-animation.ts

--- AppMovilesPRYJPC/src/app/animations/nav-animation.ts
+++ AppMovilesPRYJPC/src/app/animations/nav-animation.ts
@@ -6,28 +6,28 @@ export const enterAnimation = (baseEl: HTMLElement, opts?: any): Animation => {
     console.log('baseEl: ', baseEl)
     console.log('opts: ', opts)
 
-    const animationCtrl = new AnimationController
+    const AnimationController = new AnimationController
 
     if (opts.direction === 'forward') {
-        return animationCtrl.create()
+        return AnimationController.create()
         .addElement(opts.enteringEl)
         .duration(DURATION)
         .easing('ease-in')
         .fromTo('opacity', 0, 1)
     } else {
-            const rootAnimation = animationCtrl.create()
+            const rootAnimation = AnimationController.create()
             .addElement(opts.enteringEl)
             .duration(DURATION)
             .easing('ease-in')
             .fromTo('opacity', 0, 1)
 
-            const leavingAnimation = animationCtrl.create()
+            const leavingAnimation = AnimationController.create()
             .addElement(opts.leavingEl)
             .duration(DURATION)
             .easing('ease-out')
             .fromTo('opacity', 1, 0)
 
-            return animationCtrl.create().addAnimation([rootAnimation, leavingAnimation]);
+            return AnimationController.create().addAnimation([rootAnimation, leavingAnimation]);
     }
 
 }
